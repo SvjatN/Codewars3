3b78f9e36059e82d4968b6c0c30d6c43db587567
Svjatoslav Nabytovych <svjatnab@gmail.com>
2019-06-16 17:40:18 +0300

    add codewars task 10

---
+++ CodewarsT10.py
@@ -0,0 +1,11 @@
+
+"""
+Given two ordered pairs calculate the distance between them.
+ Round to two decimal places. This should be easy to do in 0(1) timing.
+"""
+
+import math
+
+def distance(x1, y1, x2, y2):
+    result = math.sqrt(math.pow((x2- x1),2) + math.pow((y2-y1),2))
+    return round(result,2)
