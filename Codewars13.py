fd5428f9b5bcdeca05bfc91d94eada286e55f812
Svjatoslav Nabytovych <svjatnab@gmail.com>
2019-06-19 04:01:29 +0300

    add header

--- Codewars13.py
+++ Codewars13.py
@@ -1,10 +1,11 @@
+"""Oh no, Timmy's created an infinite loop!
+ Help Timmy find and fix the bug in his unfinished For Loop!
+"""
 def create_array(n):
     res=[]
     i=1
     while i <= n: 
         res.append(i)
         i = i+1
-    print(res)
     return res
 
-print(create_array(5))
